6c3ba0c284916ec443c13b4f650ff56f6cab254a
Leo Altíssimo Neto <leoaltissimoneto@gmail.com>
2024-07-04 10:38:29 -0300

    chore: Update sd Froge start.sh script to enable extension install by webui

--- launchpad-blueprints/SD-Forge/scripts/start.sh
+++ launchpad-blueprints/SD-Forge/scripts/start.sh
@@ -40,4 +40,4 @@ exec /usr/sbin/sshd -D &
 
 nohup jupyter-lab --allow-root --ip  0.0.0.0 --NotebookApp.token='' --notebook-dir ./ --NotebookApp.allow_origin=* --NotebookApp.allow_remote_access=1 &
 
-python launch.py --listen --opt-sdp-attention
+python launch.py --listen --opt-sdp-attention --enable-insecure-extension-access
