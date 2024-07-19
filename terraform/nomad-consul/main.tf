3a5b2d62f8d7a3820b184be310f7e2910d58bb01
Yuri Hummel <yurihummel@outlook.com>
2024-07-19 14:38:03 -0300

    chore: look for versions above or equal to the version specified in main.tf

--- terraform/nomad-consul/main.tf
+++ terraform/nomad-consul/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     latitudesh = {
       source  = "latitudesh/latitudesh"
-      version = "1.1.3" # Make sure you are using the latest version published to the Terraform registry
+      version = ">= 1.1.3" # Make sure you are using the latest version published to the Terraform registry
     }
   }
 }
